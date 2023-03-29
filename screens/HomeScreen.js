cc8d5bc05ef83d1fd08fc415b1a018c6db92c920
Misha <misharexviashvili@yahoo.com>
2023-03-29 14:04:45 +0400

    Categories and CategoryCard

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,6 +8,8 @@ import {
   MagnifyingGlassIcon,
   AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
+import { ScrollView } from "react-native";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -19,35 +21,35 @@ const HomeScreen = () => {
   // TODO: Create different JSX for Android and iOS, with If Platform.OS===
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text className="text-red-500">
-        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
-          <Image
-            source={{ uri: "https://links.papareact.com/wru" }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-          />
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
 
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">
-              Delivery Now!
-            </Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-          <UserIcon size={35} color="#00CCBB" />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Delivery Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
-        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
-          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-            <MagnifyingGlassIcon color="gray" size={20} />
-            <TextInput
-              placeholder="Restaurants and cuisines"
-              keyboardType="default"
-            />
-          </View>
-          <AdjustmentsVerticalIcon color="#00CCBB" />
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
         </View>
-      </Text>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+
+      <ScrollView className="bg-gray-100">
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   );
 };
