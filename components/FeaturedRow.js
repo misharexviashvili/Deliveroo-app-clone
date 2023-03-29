905195c2057bc18daf9b4a6abd33db33bcde94cf
Misha <misharexviashvili@yahoo.com>
2023-03-29 15:31:11 +0400

    Reastaurant Card finished

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -17,7 +17,18 @@ const FeaturedRow = ({ title, description, id }) => {
         showsHorizontalScrollIndicator={false}
         className="pt-4"
       >
-        <RestaurantCard/>
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yo! Sushi"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main st"
+          short_description="Test Description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
       </ScrollView>
     </View>
   );
